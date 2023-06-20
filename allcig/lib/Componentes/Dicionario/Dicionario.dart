41d8ce7ced0507bf1ede5581add113ce4a090b8e
Antonio Macaringue <110739274+Makas-M@users.noreply.github.com>
2023-06-20 19:49:05 +0200

    17 significados no dicionario

--- allcig/lib/Componentes/Dicionario/Dicionario.dart
+++ allcig/lib/Componentes/Dicionario/Dicionario.dart
@@ -14,17 +14,23 @@ class Dicionario extends StatefulWidget {
 
 class _DicionarioState extends State<Dicionario> {
   final List<Item>  _items = [
-    Item(
-      title: 'Azimute', 
-      content: 'Azimute é a medida angular horizontal entre uma direcção de referência, geralmente o norte, e um ponto específico em um plano horizontal.'),
-    Item(title: 'Datum', content: 'É um conjunto de parâmetros que define a posição e orientação de um elipsoide de referência em relação à Terra real. É usado como base para determinar coordenadas geográficas e altitudes.'),
-    Item(title: 'Geóide', content: 'É uma superfície equipotencial do campo gravitacional terrestre que melhor se ajusta ao nível médio dos oceanos em todo o planeta. É usado como referência para medir altitudes e representa a forma "real" da Terra, levando em conta as irregularidades topográficas.'),
-    Item(title: 'Georreferenciamento', content: 'É o processo de associar coordenadas geodésicas a objectos, imagens ou dados geoespaciais. É usado para garantir a precisão e a consistência dos dados geográficos.'),
-    Item(title: 'Triangulação', content: 'É um método de levantamento geodésico que envolve a medição de ângulos e distâncias entre pontos para determinar suas coordenadas geodésicas.'),
-    Item(title: 'Item 3', content: 'Conteúdo do Item 3'),
-    Item(title: 'Item 3', content: 'Conteúdo do Item 3'),
-    Item(title: 'Item 3', content: 'Conteúdo do Item 3'),
-    Item(title: 'Item 3', content: 'Conteúdo do Item 3'),
+    Item(title: 'Azimute', content: 'Medida angular horizontal entre uma direcção de referência, geralmente o norte, e um ponto específico em um plano horizontal.'),
+    Item(title: 'Batimetria', content: 'Levantamento topográfico subaquático que envolve a medição e o mapeamento da profundidade dos corpos de água, como oceanos, lagos e rios.'),
+    Item(title: 'Datum', content: 'Conjunto de parâmetros que define a posição e orientação de um elipsoide de referência em relação à Terra real. É usado como base para determinar coordenadas geográficas e altitudes.'),
+    Item(title: 'Elipsoide', content: 'Modelo matemático da forma da Terra, que assume a forma de um elipsoide oblato, ou seja, um esferoide achatado nos polos e alongado no equador.'),
+    Item(title: 'Escala', content: 'Relação matemática entre as dimensões reais do terreno e as dimensões representadas no mapa.'),
+    Item(title: 'Geóide', content: 'Superfície equipotencial do campo gravitacional terrestre que melhor se ajusta ao nível médio dos oceanos em todo o planeta. É usado como referência para medir altitudes e representa a forma "real" da Terra, levando em conta as irregularidades topográficas.'),
+    Item(title: 'Georreferenciamento', content: 'Processo de associar coordenadas geodésicas a objectos, imagens ou dados geoespaciais. É usado para garantir a precisão e a consistência dos dados geográficos.'),
+    Item(title: 'Gravimetria', content: 'Medição do campo gravitacional da Terra para estudar a distribuição de massa dentro do planeta. A gravimetria é usada para determinar a forma e a densidade da Terra.'),
+    Item(title: 'Jusante', content: 'Refere-se à direção do fluxo de água rio abaixo, ou seja, na direção em que o rio segue em direção à sua foz ou deságue. Jusante é o oposto de montante.'),
+    Item(title: 'Maré', content: 'Movimento regular e periódico da água do mar causado principalmente pela atração gravitacional da Lua e, em menor grau, do Sol.'),
+    Item(title: 'Montante', content: 'Refere-se à direção do fluxo de água rio acima, ou seja, na direção contrária ao curso do rio, em direção à nascente ou à fonte do rio. Montante é o oposto de jusante.'),    
+    Item(title: 'Nivelamento', content: 'Processo de medição de altitudes relativas entre pontos na superfície da Terra, utilizando um nível de precisão. É usado para criar perfis de terreno e estabelecer referências altimétricas.'),
+    Item(title: 'Parcelamento', content: 'Processo de dividir uma área de terra em lotes menores para fins de planejamento urbano, projeto de estradas, construção de edifícios, entre outros.'),
+    Item(title: 'Raster', content: 'Formato de dados geoespaciais que representa informações como uma grade de células ou pixels. É frequentemente usado para representar imagens de satélite ou modelos de terreno.'),
+    Item(title: 'Toponímia', content: 'Estudo e a representação dos nomes de lugares em um mapa. A toponímia envolve a identificação, classificação e padronização dos nomes geográficos.'),    
+    Item(title: 'Triangulação', content: 'Método de levantamento geodésico que envolve a medição de ângulos e distâncias entre pontos para determinar suas coordenadas geodésicas.'),
+    Item(title: 'Vetor', content: 'Formato de dados geoespaciais que representa informações como pontos, linhas ou polígonos. É usado para representar objetos geográficos discretos, como estradas, rios, edifícios e áreas.'),
   ];
 
   final List<bool> _itemExpandedList = List<bool>.generate(20, (index) => false);
